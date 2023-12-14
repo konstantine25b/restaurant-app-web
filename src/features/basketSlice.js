454ae54bb9495848232dcc373ee72dc5d3999b74
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2023-12-14 14:26:39 +0400

    sdf

--- src/features/basketSlice.js
+++ src/features/basketSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { useState } from "react";
-
 
 const savedData = localStorage.getItem("basket");
 
 const initialState = {
-  items: savedData? JSON.parse(savedData) : [],
+  items: savedData ? JSON.parse(savedData) : [],
 };
 
 export const basketSlice = createSlice({
@@ -18,18 +16,20 @@ export const basketSlice = createSlice({
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
 
-      let newItems = [...state.items, action.payload].sort((a, b) => a.Title.localeCompare(b.Title));
+      let newItems = [...state.items, action.payload].sort((a, b) =>
+        a.Title.localeCompare(b.Title)
+      );
       state.items = newItems; // am metodit vtovebt rac basketshi da vamatebt kide action.payloadit axal items
-      localStorage.setItem('basket', JSON.stringify(newItems))
-      
+      localStorage.setItem("basket", JSON.stringify(newItems));
     },
 
     removeFromBasket: (state, action) => {
-      
       const index = state.items.findIndex(
-        (item) => item.Id === action.payload.Id 
+        (item) => item.Id === action.payload.Id
       ); // amit vpoulob tu item romlis amogebac gvinda basketshia
-      let newBasket = [...state.items].sort((a, b) => a.Title.localeCompare(b.Title));;
+      let newBasket = [...state.items].sort((a, b) =>
+        a.Title.localeCompare(b.Title)
+      );
 
       if (index >= 0) {
         newBasket.splice(index, 1);
@@ -40,78 +40,91 @@ export const basketSlice = createSlice({
       }
 
       state.items = newBasket;
-      localStorage.setItem('basket', JSON.stringify(newBasket))
+      localStorage.setItem("basket", JSON.stringify(newBasket));
     },
-   
-  
-  removeFromBasketWithIngredients: (state, action) => {
 
-    function areEqual(array1, array2) {
-      if (array1.length === array2.length) {
-        return array1.every((element, index) => {
-          if (element === array2[index]) {
-            return true;
-          }
-    
-          return false;
-        });
+    removeFromBasketWithIngredients: (state, action) => {
+      function areEqual(array1, array2) {
+        if (array1.length === array2.length) {
+          return array1.every((element, index) => {
+            if (element === array2[index]) {
+              return true;
+            }
+
+            return false;
+          });
+        }
+
+        return false;
       }
-    
-      return false;
-    }
-  
-      
-    const index = state.items.findIndex(
-      (item) => item.Id === action.payload.Id && areEqual(item.unCheckedIngredients,action.payload.unCheckedIngredients)
-    ); // amit vpoulob tu item romlis amogebac gvinda basketshia
-    let newBasket = [...state.items].sort((a, b) => a.Title.localeCompare(b.Title));;
 
-    if (index >= 0) {
-      newBasket.splice(index, 1);
-    } else {
-      console.warn(
-        `ver amoigeb imitoro ar ari shen basketshi es : ${action.payload.Id}`
+      const index = state.items.findIndex(
+        (item) =>
+          item.Id === action.payload.Id &&
+          areEqual(
+            item.unCheckedIngredients,
+            action.payload.unCheckedIngredients
+          )
+      ); // amit vpoulob tu item romlis amogebac gvinda basketshia
+      let newBasket = [...state.items].sort((a, b) =>
+        a.Title.localeCompare(b.Title)
       );
-    }
 
-    state.items = newBasket;
-    localStorage.setItem('basket', JSON.stringify(newBasket))
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `ver amoigeb imitoro ar ari shen basketshi es : ${action.payload.Id}`
+        );
+      }
+
+      state.items = newBasket;
+      localStorage.setItem("basket", JSON.stringify(newBasket));
+    },
   },
-},
 });
 
 // Action creators are generated for each case reducer function
-export const { addToBasket, removeFromBasket,removeFromBasketWithIngredients } = basketSlice.actions;
+export const {
+  addToBasket,
+  removeFromBasket,
+  removeFromBasketWithIngredients,
+} = basketSlice.actions;
 
 export const selectBasketItems = (state) => state.basket.items;
 
 export const selectBasketItemsWithId = (state, Id) =>
   state.basket.items.filter((item) => {
-    
-    return (item.Id === Id );
+    return item.Id === Id;
   });
-  export const selectBasketItemsWithIdAndIngredients = (state, Id, unCheckedIngredients) =>
+export const selectBasketItemsWithIdAndIngredients = (
+  state,
+  Id,
+  unCheckedIngredients
+) =>
   state.basket.items.filter((item) => {
-    
     function areEqual(array1, array2) {
       if (array1.length === array2.length) {
         return array1.every((element, index) => {
           if (element === array2[index]) {
             return true;
           }
-    
+
           return false;
         });
       }
-    
+
       return false;
     }
-    return (item.Id === Id && areEqual(item.unCheckedIngredients , unCheckedIngredients) );
+    return (
+      item.Id === Id &&
+      areEqual(item.unCheckedIngredients, unCheckedIngredients)
+    );
   });
 
 export const selectBasketTotal = (state) => {
   // console.log(state.basket.items)
-  
+
   // return state.basket.items.reduce(
   //   (total, item) => {
   //     total += item.Price;
@@ -119,17 +132,14 @@ export const selectBasketTotal = (state) => {
   //     return basketTotal;
   //   },
 
-    
   // );
   let total = 0;
-  for(let i = 0 ; i<state.basket.items.length; i++){
-   total+=Number(state.basket.items[i].Price);
-   
+  for (let i = 0; i < state.basket.items.length; i++) {
+    total += Number(state.basket.items[i].Price);
   }
 
-  let basketTotal= parseFloat(total.toFixed(2))
-  return basketTotal
- 
+  let basketTotal = parseFloat(total.toFixed(2));
+  return basketTotal;
 };
 
 export default basketSlice.reducer;
